e5b8f35b7366b1ade32e26b675209386ecd788c8
andarms <andarms@outlook.com>
2017-03-31 10:51:19 -0500

    Fix Player movement

--- src/core/GameObjects/Player.js
+++ src/core/GameObjects/Player.js
@@ -1,6 +1,13 @@
 import CONFIG from '../config';
 import {PKMN} from '../main';
 
+let DIR_VERTORS = {
+  "left":  [-1, 0], 
+  "right": [1, 0],
+  "up":    [0, -1], 
+  "down":  [0, 1] 
+}
+
 class Player extends Phaser.Sprite{
   constructor(game, gender){
     let sprite = 'trchar00' + gender;
@@ -10,10 +17,12 @@ class Player extends Phaser.Sprite{
 		this.body.collideWorldBounds = true;
     this.body.setSize(32, 32, 0, 16);
     
+    this.direction = 'down';
     this.gender = gender;
     this.moving = false;
     this.speed = 1;
     this.animationSpeed = 8;
+    this.currentTile = {x:0, y:0};
     
     
     if(this.gender == 0){
@@ -53,40 +62,52 @@ class Player extends Phaser.Sprite{
     this.x = CONFIG.TILE_SIZE * x;
     this.y = CONFIG.TILE_SIZE * y - 16;
     this.targetX = this.x;
-    this.targetY = this.y;
+    this.targetY = this.y + 16;
+    this.currentTile.x = x;
+    this.currentTile.y = y;
   }
 
   update(){
     if(!this.moving && this.game.cgo == this){
-      if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-        this.targetX += CONFIG.TILE_SIZE;
-        this.animations.play("right");
-        this.moving = true;
-      }
-      if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-        this.targetX -= CONFIG.TILE_SIZE;
-        this.animations.play("left");
-        this.moving = true;
-      }
-      if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-        this.targetY += CONFIG.TILE_SIZE;
-        this.animations.play("down");
-        this.moving = true;
-      }
       if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP)){
         this.targetY -= CONFIG.TILE_SIZE;
-        this.animations.play("up");
+        this.direction = "up";
+        this.moving = true;
+      }
+      else if(this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+        this.targetY += CONFIG.TILE_SIZE;
+        this.direction = "down";
+        this.moving = true;
+      }
+      else if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+        this.targetX -= CONFIG.TILE_SIZE;
+        this.direction = "left";
+        this.moving = true;
+      }
+      else if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+        this.targetX += CONFIG.TILE_SIZE;
+        this.direction = "right";
         this.moving = true;
       }
     }
 
-    if(this.targetX > this.x) this.x += this.speed;
-    if(this.targetX < this.x) this.x -= this.speed;
-    if(this.targetY > this.y) this.y += this.speed;
-    if(this.targetY < this.y) this.y -= this.speed;
+    // Check for collisions
+    let vector = DIR_VERTORS[this.direction];
+    if(this.moving){
+      this.currentTile.x += vector[0];
+      this.currentTile.y += vector[1];
+    }
 
-    if(this.targetX == this.x && this.targetY == this.y){
+    if(this.moving){
+      this.animations.play(this.direction);
+      this.body.x += vector[0] * this.speed;
+      this.body.y += vector[1] * this.speed;
+    }
+
+    if(this.targetX == this.body.x && this.targetY == this.body.y){
       this.moving = false;
+      this.animations.stop();
+      this.frame = this.idleFrames[this.direction]
     }
   }
 
